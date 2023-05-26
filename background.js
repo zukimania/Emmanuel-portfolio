be7f5dd3595ed6da107306bd215eff132ea521fa
zukimania <eayodele585@gmail.com>
2023-05-26 19:17:24 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,19 @@
+// var color = ["#292323",""];
+// var i = 0;
+// document.querySelector("button").addEventListener("click",
+//     function () {
+//         i = i < color.length ? ++i : 0;
+//         document.querySelector("body").style.background = color[i]
+//     }
+// )
+// function toggleBackground() {
+//     let body = document.body
+
+//     if (body.style.backgroundColor === "#292323") {
+//         // body.style.backgroundColor = "white";
+//         // body.style.color = "black"
+//     } else {
+//          body.style.backgroundColor = "#081617"
+//         body.style.color = "white"
+//     }
+// }
